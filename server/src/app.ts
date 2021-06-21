eb3329c0306b5b0d5a4beaf7230eb6b3beab160d
Amanda Pettenati <amanda@pette.net>
2021-06-21 17:51:06 -0400

    Revert "refactor app.listen & db connection to server.ts"
    This reverts commit ea9696c8ff933e6361e6ce3b8d0ed55b78b054cc.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
+import mongoose from 'mongoose'
+import logger from './utils/logger'
+import { PORT } from './utils/config'
+import { MONGO_URI } from './utils/config'
 
 export const app = express()
+
+// MongoDB connection
+mongoose.connect(`${MONGO_URI}`, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => logger.info('MongoDB database connection established successfully'))
+  .catch((error) => logger.error(`MongoDB connection error: ${error}`))
 
 // Middleware
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
@@ -19,3 +28,5 @@ app.use('/user', UsersRouter)
 app.use('/auth', AuthRouter)
 app.use('/project', ProjectsRouter)
 app.use('/', IndexRouter)
+
+app.listen(PORT, () => { logger.info(`Server running on port ${PORT}`) })
