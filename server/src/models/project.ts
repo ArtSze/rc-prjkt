3295287994c91de3932f88679f3e36eb4058f9b7
Amanda Pettenati <amanda@pette.net>
2021-06-25 13:18:17 -0400

    remove merge conflict

--- server/src/models/project.ts
+++ server/src/models/project.ts
@@ -11,18 +11,9 @@ export interface IProject {
 }
 
 const ProjectSchema = new Schema<IProject>({
-<<<<<<< HEAD
-	title: { type: String, required: true, maxLength: 160 },
-	description: {
-		type: String,
-		minLength: 20,
-		maxLength: 480,
-	},
-=======
 
 	title: { type: String, unique: true, maxLength: 160 },
 	description: { type: String, minLength: 20, maxLength: 480 },
->>>>>>> bc3ecfd5780fbb8e2c475b9c440e543c56447702
 	githubLink: { type: String },
 	owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 	collaborators: {
