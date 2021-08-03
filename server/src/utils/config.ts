5f0ab4580c20486efd76e232b0468a3494042441
Artur Szerejko <szerejko.artur@gmail.com>
2021-08-03 17:44:07 -0400

    changed build script and target prop within tsconfig

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -5,8 +5,6 @@ export const PORT = process.env['PORT'];
 // Mongo Config
 export const MONGO_URI = process.env['NODE_ENV'] === 'test' ? process.env['TEST_MONGO_URI'] : process.env['MONGO_URI'];
 
-export const DEVELOPMENT_URL = process.env['NODE_ENV'] === 'development' ? process.env['DEVELOPMENT_URL'] : '';
-
 export const CLIENT_URL = process.env['NODE_ENV'] === 'development' ? process.env['DEVELOPMENT_CLIENT_URL'] : '';
 
 // Express Session Config
