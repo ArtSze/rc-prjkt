2c6ffada080837abee74e38538f19c62b9c4adf6
Artur Szerejko <szerejko.artur@gmail.com>
2021-08-04 17:52:51 -0400

    configured CORS headers within axiosinstance

--- rc-prjkt-client/src/utils/axiosInstance.ts
+++ rc-prjkt-client/src/utils/axiosInstance.ts
@@ -3,5 +3,8 @@ import axios, { AxiosRequestConfig } from 'axios';
 export const baseURL = 'https://rc-project-jjtv5.ondigitalocean.app/api';
 export const authURL = baseURL + '/auth';
 
-const config: AxiosRequestConfig = { baseURL };
+const config: AxiosRequestConfig = {
+    baseURL,
+    headers: { 'X-Requested-With': 'XMLHttpRequest', 'Access-Control-Allow-Origin': '*' },
+};
 export const axiosInstance = axios.create(config);
