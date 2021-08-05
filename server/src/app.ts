42591afbfc155dbdf911c6d6edd33db69797f9cf
Artur Szerejko <szerejko.artur@gmail.com>
2021-08-05 15:40:32 -0400

    reverted back to cors library and commented out headers from axiosInstance config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 import helmet from 'helmet';
 import session from 'express-session';
 import MongoStore from 'connect-mongo';
@@ -11,13 +11,14 @@ export const app = express();
 // Middleware
 // app.use(express.static(path.resolve(__dirname, '../../rc-prjkt-client/build')));
 
-// app.use(cors({ credentials: true }));
-app.use(function (_, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://stoic-leavitt-9268eb.netlify.app');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', '*');
-    next();
-});
+app.use(cors({ origin: 'https://stoic-leavitt-9268eb.netlify.app', credentials: true }));
+
+// app.use(function (_, res, next) {
+//     res.header('Access-Control-Allow-Origin', 'https://stoic-leavitt-9268eb.netlify.app');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Headers', '*');
+//     next();
+// });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
