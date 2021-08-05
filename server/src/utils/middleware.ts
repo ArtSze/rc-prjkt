72f7909603ca83be3fc4a58ab4feba91b4bc9a4b
Artur Szerejko <szerejko.artur@gmail.com>
2021-08-05 13:10:28 -0400

    added log statement to cookiecheck middleware

--- server/src/utils/middleware.ts
+++ server/src/utils/middleware.ts
@@ -5,12 +5,15 @@ export const sessionCookieCheck = (req: Request, res: Response, next: NextFuncti
         'https://rc-project-jjtv5.ondigitalocean.app/api/auth',
         'https://rc-project-jjtv5.ondigitalocean.app/api/auth/callback',
     ];
+
+    console.log(req.path);
+
     if (nonSecurePaths.includes(req.path)) return next();
 
     if (req.session.user) {
         next();
     } else {
-        console.log('failing in middleware');
+        // console.log('failing in middleware');
         res.status(401)
             .send({
                 error: 'unauthorized',
