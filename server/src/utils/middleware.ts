cd10d3375a46e43c430f83ef4840177740ad368a
Artur Szerejko <szerejko.artur@gmail.com>
2021-08-05 13:52:45 -0400

    paths within cookiecheck reverted

--- server/src/utils/middleware.ts
+++ server/src/utils/middleware.ts
@@ -1,14 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const sessionCookieCheck = (req: Request, res: Response, next: NextFunction): void => {
-    const nonSecurePaths = [
-        'https://rc-project-jjtv5.ondigitalocean.app/api/auth',
-        'https://rc-project-jjtv5.ondigitalocean.app/api/auth/callback',
-    ];
+    const nonSecurePaths = ['/api/auth', '/api/auth/callback'];
 
     console.log(req.path);
 
-    if (nonSecurePaths.includes(req.path)) return next();
+    if (nonSecurePaths.includes(req.path)) next();
 
     if (req.session.user) {
         next();
