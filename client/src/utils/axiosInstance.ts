7c1bf1079adbc1d16d0b3a99be54b05b61aad65e
Amanda Pettenati <amanda@pette.net>
2021-08-12 13:43:18 -0400

    deploy

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -1,8 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { paramsSerializer } from '../utils/paramParser';
 
-console.log('node env', process.env.NODE_ENV);
-
 export const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000/api' : '/api';
 
 export const authURL = baseURL + '/auth';
