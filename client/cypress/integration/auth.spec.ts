daad594f7516a46d20351a88ef8b6af2ea8a31f5
Amanda Pettenati <amanda@pette.net>
2021-08-20 09:32:05 -0400

    Add auth page tests

---
+++ client/cypress/integration/auth.spec.ts
@@ -0,0 +1,45 @@
+/// <reference types="cypress" />
+
+describe('Authorization Page contents', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:4000');
+    });
+
+    it('contains the navigation bar with an authorize button', () => {
+        cy.get('header').within(() => {
+            cy.get('h1').should('contain', 'RC Projects');
+            cy.get('a').should('contain', 'Authorize').and('have.attr', 'href', '/api/auth');
+        });
+    });
+
+    it('contains a box with the logo, greeting, and authorization button', () => {
+        cy.get('#root').within(() => {
+            cy.get('h2').should('contain', 'Welcome to RC Projects!');
+            cy.get('img').should('have.attr', 'alt', 'logo');
+            cy.get('a').should('contain', 'Authorize').and('have.attr', 'href', '/api/auth');
+        });
+    });
+
+    it('should allow authentication', () => {
+        const user = {
+            first_name: 'test',
+            last_name: 'test',
+            rcId: 1234,
+            ownedProjects: [],
+            collabProjects: [],
+            zulip_id: 1234,
+            image_path: '',
+            batch: '',
+            batchEndDate: new Date(),
+        };
+        cy.request({
+            method: 'GET',
+            url: 'http://localhost:4000/api/auth/callback',
+            body: { user },
+        });
+        cy.visit('http://localhost:4000/');
+        cy.get('header').within(() => {
+            cy.get('button').should('contain', 'Add Project');
+        });
+    });
+});
