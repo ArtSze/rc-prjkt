3598cf0c57a5b7e3869cfaf38270154e5aaf2dd0
Amanda Pettenati <amanda@pette.net>
2021-09-22 12:10:53 -0400

    add initial middleware change to set demo user

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -5,10 +5,26 @@ export const sessionCookieCheck = (req: Request, res: Response, next: NextFuncti
 
     if (nonSecurePaths.includes(req.path)) return next();
 
+    const demoUser = {
+    first_name: 'Demo',
+    last_name: 'User',
+    rcId: 1234,
+    zulip_id: 1234,
+    ownedProjects: [],
+    collabProjects: [],
+    image_path: '',
+    batch: "W2'21",
+    batchEndDate: new Date(2020, 8, 7),
+    };
+
+    // TODO: update 'localhost' string to correct url
+    if (req.hostname === 'localhost') {
+        req.session.user = demoUser
+    }
+
     if (req.session.user) {
         return next();
     } else {
-        // console.log('401 failing in sessionCookieCheck');
         return res
             .status(401)
             .send({
